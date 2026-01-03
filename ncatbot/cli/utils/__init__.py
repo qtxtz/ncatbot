3738b031dcca0c66bfe0fa0f01bda06c5a3ad3d4
huan-yp <3051561876@qq.com>
2026-01-03 17:45:26 +0800

    clear: 删除不再需要的 pip_tools 代码

--- ncatbot/cli/utils/__init__.py
+++ ncatbot/cli/utils/__init__.py
@@ -2,10 +2,8 @@
 
 from ncatbot.cli.utils.cli_exceptions import CLIExit
 from ncatbot.cli.utils.constants import PYPI_SOURCE
-from ncatbot.cli.utils.pip_tool import install_pip_dependencies
 
 __all__ = [
     "PYPI_SOURCE",
-    "install_pip_dependencies",
     "CLIExit",
 ]
